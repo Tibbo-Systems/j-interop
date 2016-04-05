0c56f1bc612c01ffb346b693af83dd2cc4579316
Daniel Baskakov <gdaniq@gmail.com>
2016-04-05 15:19:27 +0300

    Fix wrong log level

--- j-interop/src/org/jinterop/dcom/core/JIComObjectImpl.java
+++ j-interop/src/org/jinterop/dcom/core/JIComObjectImpl.java
@@ -111,7 +111,7 @@ final class JIComObjectImpl implements IJIComObject {
 		obj.addOutParamAsType(Integer.class,JIFlags.FLAG_NULL);//Hresult for size
 		if (JISystem.getLogger().isLoggable(Level.INFO))
         {
-			JISystem.getLogger().warning("addRef: Adding 5 references for " + ptr.getIPID() + " session: " + session.getSessionIdentifier());
+			JISystem.getLogger().info("addRef: Adding 5 references for " + ptr.getIPID() + " session: " + session.getSessionIdentifier());
         }
 
 		JISession.debug_addIpids(ptr.getIPID(), 5);
@@ -142,7 +142,7 @@ final class JIComObjectImpl implements IJIComObject {
 		obj.addInParamAsInt(0,JIFlags.FLAG_NULL);//private refs = 0
 		if (JISystem.getLogger().isLoggable(Level.INFO))
         {
-			JISystem.getLogger().warning("RELEASE called directly ! removing 5 references for " + ptr.getIPID()+ " session: " + session.getSessionIdentifier());
+			JISystem.getLogger().info("RELEASE called directly ! removing 5 references for " + ptr.getIPID()+ " session: " + session.getSessionIdentifier());
 			JISession.debug_delIpids(ptr.getIPID(), 5);
         }
 //		session.getStub2().addRef_ReleaseRef(obj);
